1a5b73d2c1e0cb3627eddc4e32ca5665cf835114
salwaalnnn <salwaalnnn@student.telkomuniversity.ac.id>
2022-11-04 16:44:48 +0700

    modul 1

---
+++ MODUL1/src/Menu.java
@@ -0,0 +1,6 @@
+public class Menu {
+
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+    public 
+
+}
---
+++ MODUL1/src/Server.java
@@ -0,0 +1,34 @@
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        // TODO Create Database
+         
+        // TODO Create Menu
+        
+
+        // TODO Insert Menu to Database
+
+
+        // TODO Display Main Menu
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Selamat Datang di Restoran EAD");
+        System.out.println("Silahkan pesan menu terlebih dahulu");
+        System.out.println("====================================");
+        string nama = scanner.nextLine();
+        int nohp = scanner.nextInt();
+
+        // TODO Create User
+
+        // Display Menu
+        While (true){
+            System.out.println("1. Menu");
+            System.out.println("2. Pilih Menu");
+            System.out.println("3. Keluar");
+            System.out.println("====================================");
+        }
+    }
+}
